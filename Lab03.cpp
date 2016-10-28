1bfd8a9ca31edd78be60485657ff0c07e1abe79f
Samir Facussé <samir.facusse@unitec.edu>
2016-10-28 13:36:29 -0600

    -_

--- Lab03.cpp
+++ Lab03.cpp
@@ -1,1 +1,33 @@
-Hi
+#include <iostream>
+
+using namespace.std;
+
+int main()
+{
+	
+	int op;
+	do
+	{
+		cout<<"1 - Domingos"<<endl<<"2 - División Sintética"<<endl<<"3 - Permutaciones"<<endl;
+		cin<<op;
+		switch()
+		{
+			case 1:
+			{
+				
+				break;
+			}
+			case 2:
+			{
+				
+				break;
+			}
+			case 3:
+			{
+				
+				break;
+			}
+		}
+	}while(op<4&&op>0);
+	return 0;
+}
